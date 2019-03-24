f9ac8b95b43b19d0edea54ee90151e41d6ca80c3
Markus Eckstein <markus@markuseckstein.de>
2019-03-24 19:55:12 +0100

    ci: add deploy actions

---
+++ actions/jekyll/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
+bundle install > /dev/null 2>&1
+bundle list | grep "jekyll ("
+echo 'Cleaning the yokes. '
+bundle exec jekyll clean
+echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
+bundle exec jekyll build
+echo '👍 THE SITE IS BUILT'
+
